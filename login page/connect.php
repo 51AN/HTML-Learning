aa71999568f2da3009303b44fd8fdd1e9335079e
51AN <sianashsad@iut-dhaka.edu>
2022-09-14 01:20:50 +0600

    update login

---
+++ login page/connect.php
@@ -0,0 +1,29 @@
+<?php
+
+    $username = '';
+    $email = '';
+    $password = '';
+    if(isset($_POST['submit'])){
+        $username=$_POST['username'];
+        $email=$_POST['email'];
+        $password=$_POST['password'];
+    }
+
+    $conn = new mysqli('localhost','root','','spl');
+
+    if($conn -> connect_error){
+        die('Connection Failed : ' .$conn->connect_error);
+    }
+    else{
+        // $SQL="INSERT INTO `users`(`username`, `email`, `password`) VALUES ('$username','$email','$password')";
+        // $result=mysqli_query($conn,$SQL);
+        $stmt = $conn->prepare("insert into users(username,email,password) values(?,?,?)");
+        $stmt ->bind_param("sss",$username, $email, $password);
+        $stmt -> execute();
+        echo "Logged in successfully!";
+        $stmt -> close();
+        $conn -> close();
+    }
+
+
+?>
